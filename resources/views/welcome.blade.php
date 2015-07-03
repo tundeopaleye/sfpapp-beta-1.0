e5c06282c7ce83b657ed6948713e3e4ade41b26c
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-07-03 17:27:21 +0100

    banner + home adjustments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,14 @@
 
 @section('content')
 
-	
+<title>Stories From Pictures Home</title>	
 <div id="wrapper">
       
         <div class="slider-wrapper theme-default">
             <div id="slider" class="nivoSlider">
-                <img src="images/sfpbanner1.jpg" data-thumb="images/sfpbanner1.jpg" alt="" />
-                <img src="images/sfpbanner2.jpg" data-thumb="images/sfpbanner2.jpg" alt="" data-transition="slideInLeft" />
-                <img src="images/sfpbanner1.jpg" data-thumb="images/sfpbanner1.jpg" alt="" title="#htmlcaption" />
+                <img src="images/sfpbanner1.jpg" data-thumb="images/sfpbanner1.jpg" alt="" data-transition="fade" />
+                <img src="images/sfpbanner2.jpg" data-thumb="images/sfpbanner2.jpg" alt="" data-transition="fade" />
+                <img src="images/sfpbanner3.jpg" data-thumb="images/sfpbanner3.jpg" alt="" data-transition="fade" />
             </div>
            <!-- <div id="htmlcaption" class="nivo-html-caption">
                 <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>. 
