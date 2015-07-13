bdf431e375b7f904591382fea4f4f4ef46c6f66e
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-07-13 08:15:46 +0100

    collapse navbar

--- resources/views/layouts/partials/mainnav.blade.php
+++ resources/views/layouts/partials/mainnav.blade.php
@@ -1,2 +1,46 @@
-<div align="center"><a href="/" class="btn btn-warning">HOME</a>  <a href="/stories/" class="btn btn-warning">STORIES</a>  <a href="/stories/create"  class="btn btn-warning">CREATE STORY</a> <a href="/captions/" class="btn btn-warning">CAPTIONS</a>  <a href="/captions/create"  class="btn btn-warning">CREATE CAPTION</a> <a href="/brands"  class="btn btn-warning">BRAND STORIES</a>  <a href="/brands/create"  class="btn btn-warning">CREATE BRAND STORY</a> <a href="/logout"  class="btn btn-warning">LOG OUT</a>  </div>
-	
+<!--<div align="center"><a href="/" class="btn btn-warning">HOME</a>  <a href="/stories/" class="btn btn-warning">STORIES</a>  <a href="/stories/create"  class="btn btn-warning">CREATE STORY</a> <a href="/captions/" class="btn btn-warning">CAPTIONS</a>  <a href="/captions/create"  class="btn btn-warning">CREATE CAPTION</a> <a href="/brands"  class="btn btn-warning">BRAND STORIES</a>  <a href="/brands/create"  class="btn btn-warning">CREATE BRAND STORY</a> <a href="/logout"  class="btn btn-warning">LOG OUT</a>  </div>
+-->
+
+ <!-- Static navbar -->
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+           </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+            	
+              <li class="active"><a href="/">Home</a></li>
+              <li><a href="../stories">Stories</a></li>
+              <li><a href="./stories/create">Create Stories</a></li>
+               <li><a href="../captions">Captions</a></li>
+              <li><a href="./captions/create">Create Captions</a></li>
+               <li><a href="../brands">Brand Stories</a></li>
+              <li><a href="./brands/create">Create Brand Stories</a></li>
+             <!-- <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+             -->
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+             
+              <li><a href="../dashboard">Dashboard</a></li>
+              <li><a href="../logout">Logout</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>	
