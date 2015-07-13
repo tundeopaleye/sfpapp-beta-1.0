313b8707a2148a39543fe81da60ded56329d8cb4
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-07-13 08:19:31 +0100

    remove username bracket

--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -22,7 +22,7 @@
 				<div id="grid" class="col-md-3 col-sm-6"padding-bottom: 3em;"> 
   				<br><br>
 	            <p class="orange"><h3>{{ $brand->title }}</h3></p>
-	           <h5 style="color: #f57f20;">Written by: {{ $brand->user->name }}( ({{ $brand->user->username }}))</h5>
+	           <h5 style="color: #f57f20;">Written by: {{ $brand->user->name }}</h5>
 	            <p><div style="height:10em; overflow: hidden; border:3px solid #f57f20; "><a href="/brands/{{$brand->id}}">{!!HTML::image("thumbnails/$brand->thumbnail",'', array('width'=>'100%','height'=>'auto')) !!}</a></div></p>
 	            <p>{{ str_limit($brand->brand, $limit = 250, $end = '...') }}</p>
 	
