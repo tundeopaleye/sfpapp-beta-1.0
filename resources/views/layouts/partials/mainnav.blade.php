472d2506144275324ab87636811ff993a77b5b1d
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-07-14 05:48:19 +0100

    correcting links

--- resources/views/layouts/partials/mainnav.blade.php
+++ resources/views/layouts/partials/mainnav.blade.php
@@ -15,13 +15,13 @@
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
             	
-              <li><a href="../">Home</a></li>
-              <li><a href="../stories">Stories</a></li>
-              <li><a href="../stories/create">Create Story</a></li>
-               <li><a href="../captions">Captions</a></li>
-              <li><a href="../captions/create">Create Caption</a></li>
-               <li><a href="../brands">Brand Stories</a></li>
-              <li><a href="../brands/create">Create Brand Story</a></li>
+              <li><a href="/">Home</a></li>
+              <li><a href="/stories">Stories</a></li>
+              <li><a href="/stories/create">Create Story</a></li>
+               <li><a href="/captions">Captions</a></li>
+              <li><a href="/captions/create">Create Caption</a></li>
+               <li><a href="/brands">Brand Stories</a></li>
+              <li><a href="/brands/create">Create Brand Story</a></li>
              <!-- <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
                 <ul class="dropdown-menu">
