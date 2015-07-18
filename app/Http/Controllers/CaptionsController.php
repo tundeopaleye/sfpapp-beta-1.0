e30545e376436e33c4ddd35d0ac56f88b7315323
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-07-18 11:05:44 +0100

    more s3 tests

--- app/Http/Controllers/CaptionsController.php
+++ app/Http/Controllers/CaptionsController.php
@@ -55,8 +55,8 @@ class CaptionsController extends Controller {
 		//$imagepath = \Storage::disk('s3')->get(public_path());
        // $contents = \Storage::disk('s3')->get($path2b.$filet);
       //  $contents = \Storage::disk('s3')->get($path2b);
-      $contents = \Storage::disk('s3')->get('https://sfpapp.s3.amazonaws.com/1437146034-sfp.jpg');
-		return view('captions.index')->with('captions', Caption::orderBy('id','DESC')->paginate(12))->with('contents', $contents); //Temporary paginate 4
+     // $contents = \Storage::disk('s3')->get('https://sfpapp.s3.amazonaws.com/1437146034-sfp.jpg');
+		return view('captions.index')->with('captions', Caption::orderBy('id','DESC')->paginate(12)); //Temporary paginate 4
 			
 			
 			
