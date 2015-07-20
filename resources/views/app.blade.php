a99f11e2e074c39acec82696514dc7b36c7222e1
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-07-20 13:00:08 +0100

    include home on login

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -43,6 +43,7 @@
 
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
+						<li><a href="{{ url('/') }}">Home</a></li>
 						<li><a href="{{ url('/login') }}">Login</a></li>
 						<li><a href="{{ url('/register') }}">Register</a></li>
 					@else
