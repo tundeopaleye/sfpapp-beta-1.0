f5432425d364e8f215ef4aef81253aa8a8492523
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-07-22 09:02:41 +0100

    slide arrow edits

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -155,8 +155,8 @@
     $(window).load(function() {
         $('#slider').nivoSlider(
         	{   
-    prevText: '<img src="https://sfpapp.s3.amazonaws.com/images/left-arrow.png">',                 // Prev directionNav text
-    nextText: '<img src="https://sfpapp.s3.amazonaws.com/images/right-arrow.png">',                 // Next directionNav text    
+    prevText: '<img src="https://sfpapp.s3.amazonaws.com/images/left-arrow2.png">',                 // Prev directionNav text
+    nextText: '<img src="https://sfpapp.s3.amazonaws.com/images/right-arrow2.png">',                 // Next directionNav text    
 }
         );
     });
