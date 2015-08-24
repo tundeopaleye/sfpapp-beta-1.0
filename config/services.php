97dc071fd83888411a52f4995774afdebd9e9fad
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-08-24 16:32:40 +0100

    fb soc

--- config/services.php
+++ config/services.php
@@ -48,7 +48,7 @@ return [
 	'facebook' => [
 	'client_id' => env('FACEBOOK_CLIENT_ID'),
 	'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-	'redirect' => 'http://pictolit.com',
+	'redirect' => 'http://pictolit.com/social/login/facebook',
 	],
 	
 	'github' => [
