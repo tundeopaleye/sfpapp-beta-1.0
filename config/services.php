3606e0ac783a768477bf47f03f0c3d7291b93739
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-08-24 16:27:47 +0100

    social login fb tests

--- config/services.php
+++ config/services.php
@@ -48,7 +48,7 @@ return [
 	'facebook' => [
 	'client_id' => env('FACEBOOK_CLIENT_ID'),
 	'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-	'redirect' => 'http://sfpapp.com/social/login/facebook',
+	'redirect' => 'http://pictolit.com',
 	],
 	
 	'github' => [
