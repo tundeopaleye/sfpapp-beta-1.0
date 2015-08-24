2faa03ed6011e74bab9d604ec5f11482b9989836
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-08-24 12:56:30 +0100

    attempt social login fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,6 @@
 	
 	<link href='http://fonts.googleapis.com/css?family=Special+Elite' rel='stylesheet' type='text/css'>
 
-
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
