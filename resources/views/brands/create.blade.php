9c42cb0d998a329ed490fb33fb3366b57d228b3a
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-09-21 15:36:24 +0100

    remove styling

--- resources/views/brands/create.blade.php
+++ resources/views/brands/create.blade.php
@@ -42,7 +42,7 @@ array('multiple'=>'multiple','name'=>'categories[]')) !!}
 
 <div>
 	Tags<br>
-	{!! mention()->asText('recipient', old('recipient'), 'users', 'name', 'form-control') !!} <!---->
+	{!! mention()->asText('recipient', old('recipient'), 'users', 'name') !!} <!---->
 </div>
 
 <div class="form-group">
