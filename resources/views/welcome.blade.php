a9d81450b9c73aa8b3c8d00f36a1d5b94d3b2339
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-10-08 04:06:43 +0100

    footer move?

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -133,7 +133,10 @@
         		
         		</div>
         	
-        	
+        	<div class="col-md-12" style="background-color: #000; color: #fff; padding:1em; font-size: 10px; margin-top:1em;" align="center">
+	&copy; 2015 Pictolit - All Rights Reserved 
+
+</div>
         	
         </div>
         
