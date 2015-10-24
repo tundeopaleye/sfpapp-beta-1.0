5583cba3cf0e35ba8b0f35ab6ad519190e9ffdd5
Tunde Opaleye <olatundeopaleye@gmail.com>
2015-10-24 22:02:24 +0100

    text edit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -30,7 +30,7 @@
 						
 						<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
 						<div class="form-group">
-							<label class="col-md-4 control-label">Userame</label>
+							<label class="col-md-4 control-label">Username</label>
 							
 							<div class="col-md-6">
 								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
